9d3e1414ea94eac414608530dfb4b5b1b01bf623
hel <119044183+zoro1801@users.noreply.github.com>
2025-02-24 05:52:08 +0530

    Delete LaptopAdvisor directory

--- LaptopAdvisor/advisor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import select_model,predict,home
-
-urlpatterns = [
-    path("", home, name="home"),
-    path("select_model/<str:model_name>/", select_model, name="select_model"),
-    path("predict/", predict, name="predict"),
-]
